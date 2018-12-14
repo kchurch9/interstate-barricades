d032f3f93dcff68a65f6c34ea29bc0e8b2dd1e1d
Ken Church <30963709+kchurch9@users.noreply.github.com>
2018-12-13 22:27:47 -0700

    header style changes

---
+++ src/Body.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import "./Body.css"
+
+export default class Body extends Component {
+  render() {
+    return (
+      <div>
+		  This is the body
+	  </div>
+    )
+  }
+}
